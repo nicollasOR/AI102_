e275b11ca872429a6de00450185b8b80cfced9d4
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-05-20 17:20:49 -0300

    resolvendo exercicios de laço

---
+++ lacos-repeticao/for/001.py
@@ -0,0 +1,42 @@
+# print("--- ---")
+# print(f"contagem de 1 a 10 abaixo ")
+# for i in range(1,11):
+#     print(i)
+
+# for contador2 in range(1, 10):
+#     contador = 1
+#     while contador == 1:
+#         print(contador2)
+#         contador = contador + 1        
+#         if contador > contador2:
+#             print("O contador 1 é maior que o 2")
+#         elif contador <= contador2:
+#             print("O contador 2 é maior que o 1")
+
+# for i in range(1, 13):
+#     # print({i % 2 == 0})
+#     if i % 2 == 0:
+#         print(f"o numero ", i, " é par")
+
+# for i in range(1, 5):
+    # print("Programando em Python!")
+
+# for i in range(3, 1):
+#     if i > 1:
+#         print("Decolou!")
+
+while True:
+    for i in range(1, 1000):
+        print("Rodando...")
+    print("Bora quebrar?\n")
+    break
+
+# while True:
+#     comando = input("Digite sacar para dinheiro ou sair para ir embora ").lower()
+#     if comando == "sacar":
+#         print("Liberando notas...")
+#     elif comando == "sair":
+#         print("Saindo ...")
+#         break
+#     else:
+#         print("ferrou")
